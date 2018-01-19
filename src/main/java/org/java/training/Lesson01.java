3e660c2e0d7d12ca8aa01686151760e4ceabcdf9
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-19 15:16:16 +0100

    Complete lambda expressions and method references exercises

--- src/main/java/org/java/training/Lesson01.java
+++ src/main/java/org/java/training/Lesson01.java
@@ -6,32 +6,48 @@ import java.util.*;
  * Lambda Expressions and Method References
  *
  * @author Speakjava (Simon Ritter)
+ * @author Solange U. Gasengayire
  */
 public class Lesson01 {
 
     /**
-     * Run the exercises to ensure we got the right answers
+     * Tester method for our class
+     * @param args command-line arguments
      */
-    public void runExercises() {
-        System.out.println("JDK 8 Lambdas and Streams MOOC Lesson 1");
-        System.out.println("Running exercise 1 solution...");
-        exercise1();
-        System.out.println("Running exercise 2 solution...");
-        exercise2();
-        System.out.println("Running exercise 3 solution...");
-        exercise3();
-        System.out.println("Running exercise 4 solution...");
-        exercise4();
-        System.out.println("Running exercise 5 solution...");
-        exercise5();
+    public static void main(String... args) {
+        Lesson01 homework = new Lesson01();
+        homework.runExercises();
     }
 
     /**
+     * Run the exercises to ensure we got the right answers
+     */
+    private void runExercises() {
+        System.out.println("\nJDK 8 Lambdas and Streams MOOC Lesson 1");
+
+        System.out.println("\nRunning exercise 1 solution...");
+        exercise1();
+
+        System.out.println("\n\nRunning exercise 2 solution...");
+        exercise2();
+
+        System.out.println("\n\nRunning exercise 3 solution...");
+        exercise3();
+
+        System.out.println("\n\nRunning exercise 4 solution...");
+        exercise4();
+
+        System.out.println("\n\nRunning exercise 5 solution...");
+        exercise5();
+    }
+
+    /*
      * All exercises should be completed using Lambda expressions and the new
      * methods added to JDK 8 where appropriate. There is no need to use an
      * explicit loop in any of the code. Use method references rather than full
      * lambda expressions wherever possible.
      */
+
     /**
      * Exercise 1
      *
@@ -43,6 +59,9 @@ public class Lesson01 {
                 "alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
 
         /* YOUR CODE HERE */
+        final StringBuilder result = new StringBuilder();
+        list.forEach(element -> result.append(element.charAt(0)));
+        System.out.print(result);
     }
 
     /**
@@ -55,6 +74,8 @@ public class Lesson01 {
                 "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
 
         /* YOUR CODE HERE */
+        list.removeIf(element -> element.length() % 2 != 0);
+        list.forEach(System.out::print);
     }
 
     /**
@@ -67,6 +88,7 @@ public class Lesson01 {
                 "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
 
         /* YOUR CODE HERE */
+        list.forEach(element -> System.out.print(element.toUpperCase() + " "));
     }
 
     /**
@@ -82,6 +104,9 @@ public class Lesson01 {
         map.put("a", 1);
 
         /* YOUR CODE HERE */
+        final StringBuilder result = new StringBuilder();
+        map.forEach((key, value) -> result.append(key).append(value));
+        System.out.print(result);
     }
 
     /**
@@ -93,16 +118,7 @@ public class Lesson01 {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         /* YOUR CODE HERE */
-    }
-
-    /**
-     * Main entry point for application
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Lesson01 lesson = new Lesson01();
-        lesson.runExercises();
+        new Thread(() -> list.forEach(element -> System.out.print(element + " "))).start();
     }
 
 }
